d81e64393f3a724ad26752d67ce94c745c62926f
jiashi-feng <shij5412@gmail.com>
2025-06-05 16:48:35 +0800

    Five

---
+++ app/src/main/java/com/greendrading/app/ui/publish/ListingFragment.kt
@@ -0,0 +1,28 @@
+package com.greendrading.app.ui.publish
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.greendrading.app.R
+
+class ListingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_listing, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.btn_back).setOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+} 
