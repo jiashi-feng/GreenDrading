02723175618db0bafb9267545886a6a7fd5fe4cc
wang <wkaty580@gmail.com>
2025-06-07 18:30:18 +0800

    l2

--- app/src/main/java/com/greendrading/app/SearchFragment.kt
+++ app/src/main/java/com/greendrading/app/SearchFragment.kt
@@ -5,23 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 
-class HomeFragment : Fragment() {
+class SearchFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        view.findViewById<View>(R.id.searchButton).setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
-        }
+        // 初始化搜索页面的逻辑
     }
 } 
